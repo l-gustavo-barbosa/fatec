99785ccb85ded2d84db9aff4ace51ce5a3b0f024
Luis <luis.pereira51@fatec.sp.gov.br>
2024-02-27 18:40:13 -0300

    deletando bd

--- web2/aula2/src/index.ts
+++
@@ -1,9 +0,0 @@
-import express, {Request, Response} from "express";
-
-const app = express();
-const PORT = 3001;
-function message(){
-    console.log(`Iniciando Servidor na porta: ${PORT}`)
-}
-app.listen(PORT,message);
-app.get('/', function(req:Request, res:Response){res.send("index.html")} );
